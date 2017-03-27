8cbe99adc8f3f07b84b0d3c4c72e34e724930a8b
Ben Miller <benjaminmiller229@gmail.com>
2017-03-27 15:03:42 -0400

    Update assignment3.c

--- assignment3.c
+++ assignment3.c
@@ -9,19 +9,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
-//Function definitions
+// Function definitions
+int FCFS(int requests[], int position, int direction);
+int SSTF(int requests[], int position, int direction);
+int SCAN(int requests[], int position, int direction);
+int LOOK(int requests[], int position, int direction);
 
 int main(int argc, char* argv []){
 
         // Ensure correct commandline arguments 
-        if(argc != 3){ printf("ERROR: Incorrect # of arguments. [Initial Disk Head Position] [LEFT | RIGHT]\n");
-                return -1;}
+        if(argc != 3){ 
+            printf("ERROR: Incorrect # of arguments. [Initial Disk Head Position] [LEFT | RIGHT]\n");
+            return -1;
+        }
 
         // Arbitrary disk # positions
-        int[] requests = {98, 183, 37, 122, 14, 124, 65, 67};
+        int requests[8] = {98, 183, 37, 122, 14, 124, 65, 67};
+        int init_pos = *argv[1];
+        int direction;
+
+        // left == 0, right == 1
+        if(argv[2] == "left"){
+            direction = 0;
+        }
+        else{
+            direction = 1;
+        }
 
         return 0;
 }
 
-    
+
+int FCFS(int requests[], int position, int direction){
+
+    return 0;
+}
+
+int SSTF(int requests[], int position, int direction){
+    return 0;
+}
+
+int SCAN(int requests[], int position, int direction){
+    return 0;
+}
+
+int LOOK(int requests[], int position, int direction){
+    return 0;
+}
