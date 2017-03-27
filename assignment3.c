40f463f799024656be6d7f051d9b13bdff1e13b4
Ben Miller <benjaminmiller229@gmail.com>
2017-03-27 14:42:11 -0400

    Create assignment3.c

---
+++ assignment3.c
@@ -0,0 +1,27 @@
+/* Disk Scheduler Simulator - 3SH3 Assignment 3
+ * ==============================================
+ *
+ *
+ * By: Benjamin D. Miller
+ *      001416516
+ *      millebd
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Function definitions
+
+int main(int argc, char* argv []){
+
+        // Ensure correct commandline arguments 
+        if(argc != 3){ printf("ERROR: Incorrect # of arguments. [Initial Disk Head Position] [LEFT | RIGHT]\n");
+                return -1;}
+
+        // Arbitrary disk # positions
+        int[] requests = {98, 183, 37, 122, 14, 124, 65, 67};
+
+        return 0;
+}
+
+    
